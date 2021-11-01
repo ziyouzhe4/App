ffc64db82f27eeae3185169bd13132d7ee5075cb
majianjie <majianjie@didiglobal.com>
2021-11-01 11:50:20 +0800

    update

---
+++ fastlane/module_branch_merge.rb
@@ -0,0 +1,16 @@
+
+lane :module_branch_merge do |options|
+
+    workspace = options[:workspace]
+    app = options[:app]
+    git_repo = options[:git_repo]
+    source_branch = options[:source_branch]
+    target_branch = options[:target_branch]
+
+    UI.important(" workspace: #{workspace}")
+    UI.important(" app: #{app}")
+    UI.important(" git_repo: #{git_repo}")
+    UI.important(" source_branch: #{source_branch}")
+    UI.important(" target_branch: #{target_branch}")
+
+  end
